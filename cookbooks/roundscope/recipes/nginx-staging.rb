5c79cf4a4c74c74a9c48d75a8f6acdc274898e5c
Alexander Obozinsky <a.obozinsky@roundscope.com>
2013-11-14 14:11:16 +0200

    [#60678452] cookbook updates to get nginx configs (both staging and production) from sysconf repo

--- cookbooks/roundscope/recipes/nginx-staging.rb
+++ cookbooks/roundscope/recipes/nginx-staging.rb
@@ -7,12 +7,35 @@
 # All rights reserved - Do Not Redistribute
 #
 
-remote_directory "/etc/nginx" do
-    source "config/nginx_staging"
-    files_owner "root"
-    mode 0755
+git "/home/deploy/chef-sysconf" do
+    action :export
+    repository "git@github.com:roundscope/chef-sysconf.git"
+    reference "staging"
+end
+
+directory "/etc/nginx/conf.d/" do
+    only_if { Dir.exists?("/etc/nginx/conf.d") }
+    not_if { File.symlink?("/etc/nginx/conf.d") }
+    action :delete
+    recursive true
+end
+
+directory "/etc/nginx/sites-available" do
+    only_if { Dir.exists?("/etc/nginx/sites-available") }
+    not_if { File.symlink?("/etc/nginx/sites-available") }
+    action :delete
+    recursive true
+end
+
+link "/etc/nginx/conf.d" do
+    to "/home/deploy/chef-sysconf/mapidy/nginx/conf.d/"
+end
+
+link "/etc/nginx/sites-available" do
+    to "/home/deploy/chef-sysconf/mapidy/nginx/sites-available/"
 end
 
 service "nginx" do
-    action :restart
+  action :restart
 end
+
